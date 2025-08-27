c5e6cb34819c6ca344c6b96348e69614837137bc
Allan Mwenja <mwenjaallan18@gmail.com>
2025-08-27 11:58:27 +0300

    User AUthentication added

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,14 @@ import { Routes, Route} from 'react-router-dom';
 const App = () => {
   return (
     <main className ="flex h-screen">
+        <Routes>
+            {/* public routes*/}
+            <Route path = "/sign-in" element={<SigninForm />} />
 
+            {/*private routes*/}
+            <Route index element={<Home />}/>
+
+        </Routes>
     </main>
   )
 }
